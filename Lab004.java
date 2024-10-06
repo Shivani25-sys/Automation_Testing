d09f6f55a8acf3f3ac464f73571aff7704f14f82
Shivani25-sys <82534440+Shivani25-sys@users.noreply.github.com>
2024-10-06 15:55:37 +0530

    Add files via upload

--- Lab004.java
+++ Lab004.java
@@ -1,6 +1,30 @@
-public class Lab004 {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
+package sept.ex_04092024;
 
+public class Lab004 {
+    public static void main (String[] args) {
+        System.out.println("Hello world!");
+        int v1 = 9;
+        int v2 = 10;
+        int v3 = 19;
+        int sum = v1+v2+v3;
+        long v7 = 23454323456L;
+        float v4 = 101.1f;
+        double v5 = 101.111d;
+        boolean g = true;
+        String v6 = "Harry";
+        System.out.print("The sum of numbers is ");
+        System.out.println(v1+v2+v3);
+        System.out.println(v1);
+        System.out.println(v2);
+        System.out.println(v4);
+        System.out.println(v5);
+        System.out.println(v6);
+        /*
+        Write
+        anything
+        This will not be printed
+        */
+        // This is single line comment
     }
-}
+
+};

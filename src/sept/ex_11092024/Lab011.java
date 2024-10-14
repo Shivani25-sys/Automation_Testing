b56f4c375822e8d64da18e1bdee5db162a82c490
Shivani <shivanisy25@gmail.com>
2024-10-14 21:06:02 +0530

    Initial commit

---
+++ src/sept/ex_11092024/Lab011.java
@@ -0,0 +1,13 @@
+package sept.ex_11092024;
+
+public class Lab011 {
+    public static void main(String[] args)
+    {
+        System.out.println("Print Ln");
+        boolean a1 = true;
+        System.out.println(a1);
+
+    }
+
+    }
+
